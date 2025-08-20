9de7e66e5974eed4587ce30cc4fc6a3efa4bfea9
Kisegami <kisegami3012@gmail.com>
2025-08-21 01:59:10 +0700

    Put Event Handling functions to another file

--- utils/scenario.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-
-def ura():
-  race_btn = pyautogui.locateCenterOnScreen("assets/ura/ura_race_btn.png", confidence=0.8, minSearchTime=0.2)
-  if race_btn:
-    pyautogui.click(race_btn)
